3f6d9973a2f1a39a92576b592a28490245ccd5d1
dffdzkwn <dffdzkwn@gmail.com>
2024-12-13 22:42:59 +0700

    add 59

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ menu = st.sidebar.radio("Pilih Menu:", ["Nama Anggota", "Isi Website"])
 
 # Menu 1: Nama Anggota
 if menu == "Nama Anggota":
-    st.header("Daftar Anggota dan Jobdesk")
+    st.header("Daftar Anggota")
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
